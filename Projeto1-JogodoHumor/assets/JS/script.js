89cbcd8bed47a26c2de2208d17f132009aa982aa
Flavio Wu <flaviowu@gmail.com>
2021-07-16 03:04:36 -0300

    finalização v1

--- Projeto1-JogodoHumor/assets/JS/script.js
+++ Projeto1-JogodoHumor/assets/JS/script.js
@@ -1,13 +1,18 @@
 function mudarHumor1(){
-    let status = document.getElementById("m1").innerHTML;
+    let status = document.getElementById("age").innerHTML;
     console.log(status)
-    if (status === "Humor1") {
-        document.getElementById("m1").innerHTML = "Humor2";
-        document.getElementById("img1").src="assets/img/Portrait_Placeholder_neg.png";
-        document.body.style.backgroundColor = "#000000";
+    if (status === "19") {
+        document.getElementById("age").innerHTML = "319";
+        document.getElementById("img1").src="assets/img/ura_old.gif";
+        document.getElementById("b1").value = "Voltar ao Começo";
+        document.getElementById("lenda6").innerHTML = 'Tomado de grande tristeza, foi para a beira do mar na esperança de reencontrar a tartaruga,mas desesperou-se porque esta demorava e acabou abrindo a caixa que a princesa lhe havia oferecido. De dentro dela saiu uma nuvem de fumaça branca, que o envolveu. De repente, seu corpo tornou-se velho e enrugado, nasceu-lhe uma longa barba branca e suas costas curvaram-se com o peso de tantos anos. E do mar veio a voz doce e triste da princesa: "Eu lhe disse para não abrir a caixa. Nela estavam todos os seus anos …" A caixa continha a "eterna juventude" de Urashima Taro e o pescador, sem reconhecer seu valor, deixou-a ir-se para sempre';
+        window.scrollTo(0, document.body.scrollHeight);
     }
-    else if (status === "Humor2") {
-        document.getElementById("m1").innerHTML = "Humor1";
-        document.getElementById("img1").src="assets/img/Portrait_Placeholder.png";
+    else if (status === "319") {
+        document.getElementById("age").innerHTML = "19";
+        document.getElementById("img1").src="assets/img/ura_young.gif";
+        document.getElementById("b1").value = "Abrir Caixa";
+        document.getElementById("lenda6").innerHTML = 'Clique em <span id="final">"Abrir Caixa"</span> no card de Urashima Tarô para saber o final da história';
+        window.scrollTo({ top: 0, behavior: 'smooth' });
     }
 }
