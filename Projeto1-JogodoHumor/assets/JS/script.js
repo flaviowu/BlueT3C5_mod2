844ab5a872be093fd5d6af4aa64af5c7aea961d6
Flavio Wu <flaviowu@gmail.com>
2021-07-15 23:12:39 -0300

    add design features

--- Projeto1-JogodoHumor/assets/JS/script.js
+++ Projeto1-JogodoHumor/assets/JS/script.js
@@ -1,13 +1,13 @@
-document.getElementById("b1").addEventListener("click", mudarHumor1());
-
 function mudarHumor1(){
-    let humor1 = document.getElementById("b1").innerHTML;
-    console.log(humor1)
-    document.getElementById("m1").innerHTML = "Voltou"
-    // if (humor1 === "Mudou") {
-                
-    // }
-    // else {
-    //     humor1.value = "Anormal";
-    // }
-};
+    let status = document.getElementById("m1").innerHTML;
+    console.log(status)
+    if (status === "Humor1") {
+        document.getElementById("m1").innerHTML = "Humor2";
+        document.getElementById("img1").src="assets/img/Portrait_Placeholder_neg.png";
+        document.body.style.backgroundColor = "#000000";
+    }
+    else if (status === "Humor2") {
+        document.getElementById("m1").innerHTML = "Humor1";
+        document.getElementById("img1").src="assets/img/Portrait_Placeholder.png";
+    }
+}
